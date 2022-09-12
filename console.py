7e5faaf2d9ec0adef6250c0fe393c2629c7c516f
Paul Gabriel <paulgabriel@DESKTOP-QCQN4GQ>
2022-09-12 23:53:06 +0100

    added console.py

--- console.py
+++ console.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
+"""Module for hbnb console"""
 
-import cmd, sys
+import cmd
 
 
 class HBNBCommand(cmd.Cmd):
+    """Class that defines the console commands for the hbnb console"""
+
     intro = 'Welcome to the HBNB console. Type help or ? to list commands.\n'
     prompt = '(hbnb)'
-    file = None
 
     def do_quit(self, arg):
-        'Quit command to exit the console\n'
+        """Quit command to exit the console\n"""
+
         print('Quitting hbnb console')
         return True
 
     def do_EOF(self, arg):
-        'Quit command to exit the console\n'
+        """Quit command to exit the console\n"""
+
         print('Quitting the console')
         return True
 
     def emptyline(self):
-        return
+        """Causes nothing to be executed when an empty line is run"""
+
+        return None
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
