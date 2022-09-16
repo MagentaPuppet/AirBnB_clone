4048b0527610865f5f8a949d39895ec70d45b3d6
Paul Gabriel <paulgabriel@DESKTOP-QCQN4GQ>
2022-09-16 13:20:24 +0100

    updated test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,40 +6,38 @@ from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User
 from models import storage
+import json
 
 
 class TestUser(unittest.TestCase):
     """Tests the class User"""
 
+    __file_path = "file.json"
+
     def test_is_subclass(self):
         """Tests if User is a subclass of BaseModel"""
         self.assertTrue(issubclass(User, BaseModel))
 
-    def test_email(self):
+    def test_attr(self):
         """Tests if attributes exist in User"""
         my_user = User()
-        id = my_user.id
-        self.assertTrue(hasattr(my_user, "email"))
+        my_user.first_name = "Betty"
+        my_user.last_name = "Bar"
         my_user.email = "airbnb@mail.com"
-        my_user.save()
-        my_user.reload()
-        self.assertTrue(my_user in storage.all())
-        self.assertTrue(my_user.email == "airbnb@mail.com")
-
-    def test_password(self):
-        """Tests if attributes exist in User"""
-        my_user = User()
+        my_user.password = "root"
+        self.assertTrue(hasattr(my_user, "email"))
         self.assertTrue(hasattr(my_user, "password"))
-
-    def test_first_name(self):
-        """Tests if attributes exist in User"""
-        my_user = User()
         self.assertTrue(hasattr(my_user, "first_name"))
-
-    def test_last_name(self):
-        """Tests if attributes exist in User"""
-        my_user = User()
         self.assertTrue(hasattr(my_user, "last_name"))
+        my_user.save()
+        storage.reload()
+        with open(TestUser.__file_path, 'r') as file:
+            file_data = json.load(file)
+        self.assertTrue(my_user.to_dict() in file_data.values())
+        self.assertTrue(my_user.first_name == "Betty")
+        self.assertTrue(my_user.last_name == "Bar")
+        self.assertTrue(my_user.email == "airbnb@mail.com")
+        self.assertTrue(my_user.password == "root")
 
 
 if __name__ == '__main__':
