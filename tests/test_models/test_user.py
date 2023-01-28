2889b5c8d28d865f52681343fa14f5edb2108505
Paul Gabriel <paul.gabriel@bison.howard.edu>
2023-01-28 12:11:17 +0100

    updated test_user.py testcases

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,44 +1,52 @@
 #!/usr/bin/python3
-"""Test module for the class User"""
+"""Unittest module for the User Class."""
 
 import unittest
 from datetime import datetime
-from models.base_model import BaseModel
+import time
 from models.user import User
-from models import storage
+import re
 import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
-    """Tests the class User"""
 
-    __file_path = "file.json"
+    """Test Cases for the User class."""
 
-    def test_is_subclass(self):
-        """Tests if User is a subclass of BaseModel"""
-        self.assertTrue(issubclass(User, BaseModel))
+    def setUp(self):
+        """Sets up test methods."""
+        pass
 
-    def test_attr(self):
-        """Tests if attributes exist in User"""
-        my_user = User()
-        my_user.first_name = "Betty"
-        my_user.last_name = "Bar"
-        my_user.email = "airbnb@mail.com"
-        my_user.password = "root"
-        self.assertTrue(hasattr(my_user, "email"))
-        self.assertTrue(hasattr(my_user, "password"))
-        self.assertTrue(hasattr(my_user, "first_name"))
-        self.assertTrue(hasattr(my_user, "last_name"))
-        my_user.save()
-        storage.reload()
-        with open(TestUser.__file_path, 'r') as file:
-            file_data = json.load(file)
-        self.assertTrue(my_user.to_dict() in file_data.values())
-        self.assertTrue(my_user.first_name == "Betty")
-        self.assertTrue(my_user.last_name == "Bar")
-        self.assertTrue(my_user.email == "airbnb@mail.com")
-        self.assertTrue(my_user.password == "root")
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
 
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
-if __name__ == '__main__':
+    def test_8_instantiation(self):
+        """Tests instantiation of User class."""
+
+        b = User()
+        self.assertEqual(str(type(b)), "<class 'models.user.User'>")
+        self.assertIsInstance(b, User)
+        self.assertTrue(issubclass(type(b), BaseModel))
+
+    def test_8_attributes(self):
+        """Tests the attributes of User class."""
+        attributes = storage.attributes()["User"]
+        o = User()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
+
+if __name__ == "__main__":
     unittest.main()
