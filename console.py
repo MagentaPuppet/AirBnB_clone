6d6a340b818f6dba387c30f606454136643363bd
Paul Gabriel <paul.gabriel@bison.howard.edu>
2023-01-28 17:16:21 +0100

    updated console.py

--- console.py
+++ console.py
@@ -170,19 +170,34 @@ class HBNBCommand(cmd.Cmd):
     def _precmd(self, arg):
         """Intercepts commands to test for class.syntax()"""
 
-        args = re.split(r"\.|\(|\)", arg)
-        if len(args) > 1:
-            if hasattr(self, "do_" + args[1]):
-                result = getattr(self, "do_" + args[1])
-                return result(args[0])
-            elif args[0] in storage.classes():
-                print("** class exists but an invalid command was passed **")
-            else:
-                print("** invalid syntax on method for specified class! **")
-        elif arg in storage.classes():
-            print("** class exists but no command was passed **")
+        match = re.search(r"^(\w*)\.(\w+)(?:\(([^)]*)\))$", line)
+        if not match:
+            return line
+        classname = match.group(1)
+        method = match.group(2)
+        args = match.group(3)
+        match_uid_and_args = re.search('^"([^"]*)"(?:, (.*))?$', args)
+        if match_uid_and_args:
+            uid = match_uid_and_args.group(1)
+            attr_or_dict = match_uid_and_args.group(2)
         else:
-            print("** command not found! Type help or ? to list commands **")
+            uid = args
+            attr_or_dict = False
+
+        attr_and_value = ""
+        if method == "update" and attr_or_dict:
+            match_dict = re.search('^({.*})$', attr_or_dict)
+            if match_dict:
+                self.update_dict(classname, uid, match_dict.group(1))
+                return ""
+            match_attr_and_value = re.search(
+                '^(?:"([^"]*)")?(?:, (.*))?$', attr_or_dict)
+            if match_attr_and_value:
+                attr_and_value = (match_attr_and_value.group(
+                    1) or "") + " " + (match_attr_and_value.group(2) or "")
+        command = method + " " + classname + " " + uid + " " + attr_and_value
+        self.onecmd(command)
+        return command
 
     @staticmethod
     def formatString(arg, index):
