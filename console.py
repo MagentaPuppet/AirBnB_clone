f91b6a82df3cb8c3529083d9b89650e1d3f0a072
Paul Gabriel <paul.gabriel@bison.howard.edu>
2023-01-28 17:24:02 +0100

    updated console.py

--- console.py
+++ console.py
@@ -221,6 +221,28 @@ class HBNBCommand(cmd.Cmd):
                 args[index] = args[index][1:-1]
         return args[index]
 
+    def update_dict(self, classname, uid, s_dict):
+        """Helper method for update() with a dictionary."""
+        s = s_dict.replace("'", '"')
+        d = json.loads(s)
+        if not classname:
+            print("** class name missing **")
+        elif classname not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(classname, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                attributes = storage.attributes()[classname]
+                for attribute, value in d.items():
+                    if attribute in attributes:
+                        value = attributes[attribute](value)
+                    setattr(storage.all()[key], attribute, value)
+                storage.all()[key].save()
+
     # ----- system overrides -----
     def emptyline(self):
         """Causes nothing to be executed when an empty line is run"""
